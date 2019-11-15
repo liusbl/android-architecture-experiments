afc9247bdcae921cbd76bbbce7d6b6cea669df6e
Liudvikas Šablauskas <liudvikas.sablauskas@telesoftas.com>
2019-11-16 00:09:22 +0200

    Create RxExtensions.kt#addTo

--- app/src/main/java/com/arch/experiments/common/extensions/RxExtensions.kt
+++ app/src/main/java/com/arch/experiments/common/extensions/RxExtensions.kt
@@ -1,5 +1,11 @@
 package com.arch.experiments.common.extensions
 
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 fun <T> T.toObservable() = Observable.just(this)
+
+fun Disposable.addTo(disposables: CompositeDisposable) {
+    disposables.add(this)
+}

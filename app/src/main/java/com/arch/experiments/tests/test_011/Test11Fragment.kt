cfbffa83f0b2606f6630cd1adf57aa8f0fa89459
Liudvikas Šablauskas <liudvikas.sablauskas@telesoftas.com>
2019-11-16 22:17:32 +0200

    Update test11

--- app/src/main/java/com/arch/experiments/tests/test_011/Test11Fragment.kt
+++ app/src/main/java/com/arch/experiments/tests/test_011/Test11Fragment.kt
@@ -1,24 +1,32 @@
 package com.arch.experiments.tests.test_011
 
 import android.os.Bundle
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Toast
 import com.arch.experiments.R
 import com.arch.experiments.common.BaseFragment
 import kotlinx.android.synthetic.main.test_010.*
 
-class Test11Fragment : BaseFragment(R.layout.test_010), NameModifierView {
+class Test11Fragment : BaseFragment(R.layout.test_011), NameModifierView {
     lateinit var presenter: Test11Presenter
+
+    private var textWatcher: TextWatcher? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         presenter.takeView(this)
+        nameEditText.onTextChanged { name ->
+            presenter.onNameUpdated(name)
+        }
         updateNameButton.setOnClickListener {
-            presenter.onUpdateNameClicked(nameEditText.text.toString())
+            presenter.onUpdateTextClicked()
         }
     }
 
     override fun setName(name: String) {
+        nameEditText.removeTextChangedListener(textWatcher)
         nameEditText.setText(name)
+        nameEditText.addTextChangedListener(textWatcher)
     }
 
     override fun showError(error: Throwable) {
--- app/src/main/java/com/arch/experiments/tests/test_011/Test11Presenter.kt
+++ app/src/main/java/com/arch/experiments/tests/test_011/Test11Presenter.kt
@@ -4,11 +4,18 @@ import com.arch.experiments.tests.test_011.misc.Api
 import io.reactivex.android.schedulers.AndroidSchedulers
 
 class Test11Presenter(private val api: Api) : BasePresenter<NameModifierView>() {
-    fun onUpdateNameClicked(name: String) {
-        api.update(name)
+    private var state: State = State("")
+
+    fun onNameUpdated(name: String) {
+        state = state.copy(name = name)
+    }
+
+    fun onUpdateTextClicked() {
+        api.update(state.name)
             .observeOn(AndroidSchedulers.mainThread())
             .doOnSubscribe { onView { showProgress() } }
             .subscribe({ newName ->
+                state = state.copy(name = newName)
                 onView {
                     hideProgress()
                     setName(newName)
@@ -18,4 +25,6 @@ class Test11Presenter(private val api: Api) : BasePresenter<NameModifierView>() 
             })
             .addDisposable()
     }
+
+    data class State(val name: String)
 }

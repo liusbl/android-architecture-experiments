a9a744ac26b577ea6f1a40bd48bf36f73df92243
Liudvikas Šablauskas <liudvikas.sablauskas@telesoftas.com>
2019-11-17 01:57:16 +0200

    Update layout for test28

--- app/src/main/java/com/arch/experiments/tests/test_028/Test28Fragment.kt
+++ app/src/main/java/com/arch/experiments/tests/test_028/Test28Fragment.kt
@@ -10,7 +10,7 @@ import com.arch.experiments.tests.test_028.misc.Database
 import com.arch.experiments.tests.test_028.misc.Greeter
 import kotlinx.android.synthetic.main.test_028.*
 
-class Test28Fragment : BaseFragment(R.layout.test_026) {
+class Test28Fragment : BaseFragment(R.layout.test_028) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val viewModel = Test28ViewModel(Database(), Greeter())
         viewModel.postAction(Action.ViewCreated)

9308d14cabc70b1cdce935fb05a6907fa512bad8
Liudvikas Šablauskas <liudvikas.sablauskas@telesoftas.com>
2019-11-19 22:54:48 +0200

    Plant Timber.DebugTree in MainActivity.kt

--- app/src/main/java/com/arch/experiments/home/MainActivity.kt
+++ app/src/main/java/com/arch/experiments/home/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.arch.experiments.R
 import com.arch.experiments.common.extensions.replaceFragment
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
         replaceFragment(
             R.id.fragmentContainer,
             MainFragment()
